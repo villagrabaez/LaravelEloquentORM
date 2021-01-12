9ac4dd0fdb14a6f50d85a0c16461e4b753584d69
Bernardino Villagra <bernardinovillagra@hotmail.com>
2021-01-12 20:36:13 -0300

    Select specific columns

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,20 @@ Route::get('/products', function(){
      *
      * Utilizando el metodo with y pasando como argumento la relacion
      * soluciona este inconveniente.
+     *
+     * Para mejorar aún más el tiempo en el que se realizan las consultas podemos
+     * especificar los campos que necesitamos mediante el metodo select()
      */
-    $products = App\Product::with('category')->get();
+    $products = App\Product::query()
+        ->select(['title', 'slug', 'image', 'description', 'category_id'])
+        ->with('category:id,title,slug')
+
+        // Alternativa al la linea anterior en caso de necesitar un query más complejo
+        // ->with(['category' => function($query){
+        //     $query->select('id', 'title', 'slug');
+        // }])
+
+        ->get();
 
     return view('products', compact('products'));
 });
